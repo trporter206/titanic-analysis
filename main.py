94f3b2a8a836462d9d4ed7b5a47a315302b1e0b1
trporter <torri.porter@yahoo.com>
2018-10-25 21:27:45 -0700

    small observations

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as pyplot
 
 titanic_data = pd.read_csv("titanic_data.csv")
 
+#quick data overview------------------------------------------------------------
 def overview(data):
     print('column names')
     print data.columns.values
@@ -19,4 +20,24 @@ def overview(data):
     print('non numeric columns')
     print data.describe(include=[np.object])
 
-overview(titanic_data)
+# overview(titanic_data)
+
+#compare class to survival rate. groupby is like pivot in excel-----------------
+class_survival = titanic_data[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+# print class_survival
+
+#compare sex and survival rate--------------------------------------------------
+sex_survival = titanic_data[['Sex', 'Survived']].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+# print sex_survival
+
+#compare number of siblings to survival rate------------------------------------
+sibling_count_survival = titanic_data[['SibSp', 'Survived']].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+# print sibling_count_survival
+
+#compare number of accompanying parents/children with survival------------------
+with_parent_or_child_survival = titanic_data[['Parch', 'Survived']].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+# print with_parent_or_child_survival
