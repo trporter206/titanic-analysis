69ac7ddff0eac4ef8e5baed460ad995c1b442f1e
trporter <torri.porter@yahoo.com>
2018-10-26 17:42:19 -0700

    visualizations done

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ def overview(data):
 
 # overview(titanic_data)
 
-#compare plcass, sex, sibsp, parch to survival rate-----------------------------
+#func to compare variables. v2 must be numeric----------------------------------
 def pivot(v1, v2):
     pivot_table = titanic_data[[v1, v2]].groupby([v1], as_index=False).mean().sort_values(by=v2, ascending=False)
     print('_'*40)
     print pivot_table
 
-pivot('Pclass', 'Survived')
-pivot('Sex','Survived')
-pivot('SibSp','Survived')
-pivot('Parch','Survived')
+# pivot('Pclass', 'Survived')
+# pivot('Sex','Survived')
+# pivot('SibSp','Survived')
+# pivot('Parch','Survived')
 
 #visual analysis of age and class survival--------------------------------------
 g = sns.FacetGrid(titanic_data, col='Survived')
@@ -40,4 +40,13 @@ g.map(plt.hist, 'Age', bins=20)
 grid = sns.FacetGrid(titanic_data, col='Survived', row='Pclass')
 grid.map(plt.hist, 'Age', alpha=.5, bins=20)
 grid.add_legend
-# plt.show()
+
+plot = sns.FacetGrid(titanic_data, row='Embarked')
+plot.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
+plot.add_legend()
+
+bar = sns.FacetGrid(titanic_data, row='Embarked', col='Survived')
+bar.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
+bar.add_legend()
+
+plt.show()
